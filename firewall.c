b5b402e48cb86d5640d125a3d8416714310468c0
Aidan Perry <github@aidanperry.net>
2024-07-21 20:16:25 -0500

    removed some code duplication by using inline functions

--- firewall.c
+++ firewall.c
@@ -62,7 +62,103 @@ struct {
     __uint(max_entries, 1024);
 } ipv6_firewall SEC(".maps");
 
-// count_packets atomically increases a packet counter on every invocation.
+static __always_inline int handle_action(struct flowspec_value *value, __u64 now_ns) {
+    __u64 time_diff_ns = now_ns - value->last_seen_ns;
+    __u64 time_diff_s = time_diff_ns / 1000000000;
+    __u64 current_pps = time_diff_s > 0 ? (1000000000 / time_diff_ns) : 0;
+
+    switch (value->action) {
+        case ALLOW:
+            return XDP_PASS;
+        case BLOCK:
+            return XDP_DROP;
+        case RATE_LIMIT:
+            if (current_pps > value->rate_limit_pps) {
+                return XDP_DROP;
+            } else {
+                value->last_seen_ns = now_ns;
+                return XDP_PASS;
+            }
+    }
+    return XDP_DROP;
+}
+
+static __always_inline int process_ipv4(struct xdp_md *ctx, struct ethhdr *eth, __u64 now_ns) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void *)(ip + 1) > data_end)
+        return XDP_PASS;
+
+    struct ipv4_flowspec_key key = {};
+    key.src_ip = ip->saddr;
+    key.dst_ip = ip->daddr;
+    key.protocol = ip->protocol;
+
+    if (ip->protocol == IP_PROTO_TCP) {
+        struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*ip);
+        if ((void *)(tcp + 1) > data_end)
+            return XDP_PASS;
+
+        key.src_port = tcp->source;
+        key.dst_port = tcp->dest;
+    } else if (ip->protocol == IP_PROTO_UDP) {
+        struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip);
+        if ((void *)(udp + 1) > data_end)
+            return XDP_PASS;
+
+        key.src_port = udp->source;
+        key.dst_port = udp->dest;
+    } else {
+        return XDP_PASS;
+    }
+
+    struct flowspec_value *value = bpf_map_lookup_elem(&ipv4_firewall, &key);
+    if (value) {
+        return handle_action(value, now_ns);
+    }
+    return XDP_DROP;
+}
+
+static __always_inline int process_ipv6(struct xdp_md *ctx, struct ethhdr *eth, __u64 now_ns) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+
+    struct ipv6hdr *ip6 = data + sizeof(*eth);
+    if ((void *)(ip6 + 1) > data_end)
+        return XDP_PASS;
+
+    struct ipv6_flowspec_key key = {};
+    __builtin_memcpy(key.src_ip, ip6->saddr.s6_addr, 16);
+    __builtin_memcpy(key.dst_ip, ip6->daddr.s6_addr, 16);
+    key.protocol = ip6->nexthdr;
+
+    if (ip6->nexthdr == IP_PROTO_TCP) {
+        struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*ip6);
+        if ((void *)(tcp + 1) > data_end)
+            return XDP_PASS;
+
+        key.src_port = tcp->source;
+        key.dst_port = tcp->dest;
+    } else if (ip6->nexthdr == IP_PROTO_UDP) {
+        struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip6);
+        if ((void *)(udp + 1) > data_end)
+            return XDP_PASS;
+
+        key.src_port = udp->source;
+        key.dst_port = udp->dest;
+    } else {
+        return XDP_PASS;
+    }
+
+    struct flowspec_value *value = bpf_map_lookup_elem(&ipv6_firewall, &key);
+    if (value) {
+        return handle_action(value, now_ns);
+    }
+    return XDP_DROP;
+}
+
 SEC("xdp")
 int firewall(struct xdp_md *ctx) {
     /* START COUNT PACKET */
@@ -75,111 +171,17 @@ int firewall(struct xdp_md *ctx) {
 
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
-    
+
     struct ethhdr *eth = data;
     if ((void *)(eth + 1) > data_end)
         return XDP_PASS;
 
     __u64 now_ns = bpf_ktime_get_ns();
-    
+
     if (eth->h_proto == __constant_htons(ETH_P_IP)) {
-        struct iphdr *ip = data + sizeof(*eth);
-        if ((void *)(ip + 1) > data_end)
-            return XDP_PASS;
-
-        struct ipv4_flowspec_key key = {};
-        key.src_ip = ip->saddr;
-        key.dst_ip = ip->daddr;
-        key.protocol = ip->protocol;
-
-        if (ip->protocol == IP_PROTO_TCP) {
-            struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*ip);
-            if ((void *)(tcp + 1) > data_end)
-                return XDP_PASS;
-
-            key.src_port = tcp->source;
-            key.dst_port = tcp->dest;
-        } else if (ip->protocol == IP_PROTO_UDP) {
-            struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip);
-            if ((void *)(udp + 1) > data_end)
-                return XDP_PASS;
-
-            key.src_port = udp->source;
-            key.dst_port = udp->dest;
-        } else {
-            return XDP_PASS;
-        }
-
-        struct flowspec_value *value = bpf_map_lookup_elem(&ipv4_firewall, &key);
-        if (value) {
-            __u64 time_diff_ns = now_ns - value->last_seen_ns;
-            __u64 time_diff_s = time_diff_ns / 1000000000;
-            __u64 current_pps = time_diff_s > 0 ? (1000000000 / time_diff_ns) : 0;
-
-            switch (value->action) {
-                case ALLOW:
-                    return XDP_PASS;
-                case BLOCK:
-                    return XDP_DROP;
-                case RATE_LIMIT:
-                    if (current_pps > value->rate_limit_pps) {
-                        return XDP_DROP;
-                    } else {
-                        value->last_seen_ns = now_ns;
-                        return XDP_PASS;
-                    }
-            }
-        }
-        return XDP_DROP;
+        return process_ipv4(ctx, eth, now_ns);
     } else if (eth->h_proto == __constant_htons(ETH_P_IPV6)) {
-        struct ipv6hdr *ip6 = data + sizeof(*eth);
-        if ((void *)(ip6 + 1) > data_end)
-            return XDP_PASS;
-
-        struct ipv6_flowspec_key key = {};
-        __builtin_memcpy(key.src_ip, ip6->saddr.s6_addr, 16);
-        __builtin_memcpy(key.dst_ip, ip6->daddr.s6_addr, 16);
-        key.protocol = ip6->nexthdr;
-
-        if (ip6->nexthdr == IP_PROTO_TCP) {
-            struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*ip6);
-            if ((void *)(tcp + 1) > data_end)
-                return XDP_PASS;
-
-            key.src_port = tcp->source;
-            key.dst_port = tcp->dest;
-        } else if (ip6->nexthdr == IP_PROTO_UDP) {
-            struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip6);
-            if ((void *)(udp + 1) > data_end)
-                return XDP_PASS;
-
-            key.src_port = udp->source;
-            key.dst_port = udp->dest;
-        } else {
-            return XDP_PASS;
-        }
-
-        struct flowspec_value *value = bpf_map_lookup_elem(&ipv6_firewall, &key);
-        if (value) {
-            __u64 time_diff_ns = now_ns - value->last_seen_ns;
-            __u64 time_diff_s = time_diff_ns / 1000000000;
-            __u64 current_pps = time_diff_s > 0 ? (1000000000 / time_diff_ns) : 0;
-
-            switch (value->action) {
-                case ALLOW:
-                    return XDP_PASS;
-                case BLOCK:
-                    return XDP_DROP;
-                case RATE_LIMIT:
-                    if (current_pps > value->rate_limit_pps) {
-                        return XDP_DROP;
-                    } else {
-                        value->last_seen_ns = now_ns;
-                        return XDP_PASS;
-                    }
-            }
-        }
-        return XDP_DROP;
+        return process_ipv6(ctx, eth, now_ns);
     }
 
     return XDP_PASS;
